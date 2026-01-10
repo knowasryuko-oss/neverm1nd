34874f19d3d9f299c4f852c069df58bed29a3085
knowasryuko-oss <knowasryuko@gmail.com>
2026-01-10 20:44:49 +0700

    Update automation.lua

--- ui/automation.lua
+++ ui/automation.lua
@@ -1,5 +1,5 @@
 -- /ui/automation.lua
--- Automation tab UI (9X Totem, dropdown value=Id, label=Name).
+-- Automation tab UI (9X Totem, dropdown string: Shiny, Luck, Mutation).
 
 return function(ctx, modules, tab)
     local AutoTotem = modules.auto_totem
@@ -7,16 +7,9 @@ return function(ctx, modules, tab)
     local sec = tab:Section({ Side = "Left", Collapsed = false })
     sec:Header({ Text = "9X Totem" })
 
-    -- Dropdown totem name (label=Name, value=Id)
-    local totemList = AutoTotem.GetTotemList()
-    local nameToId = {}
-    local nameList = {}
-    for _, v in ipairs(totemList) do
-        nameToId[v.label] = v.value
-        nameList[#nameList+1] = v.label
-    end
-    local selectedTotemName = nameList[1] or ""
-    local selectedTotemId = nameToId[selectedTotemName]
+    -- Dropdown totem name (list string)
+    local totemList = AutoTotem.GetTotemNameList()
+    local selectedTotemName = totemList[1] or ""
     local distance = 100
 
     sec:Dropdown({
@@ -24,11 +17,10 @@ return function(ctx, modules, tab)
         Search = true,
         Multi = false,
         Required = true,
-        Options = nameList,
+        Options = totemList,
         Default = selectedTotemName,
         Callback = function(name)
             selectedTotemName = name
-            selectedTotemId = nameToId[name]
         end
     })
 
@@ -48,8 +40,8 @@ return function(ctx, modules, tab)
         Default = false,
         Callback = function(v)
             if v then
-                if AutoTotem and AutoTotem.Start and selectedTotemId then
-                    AutoTotem.Start(ctx, selectedTotemId, distance)
+                if AutoTotem and AutoTotem.Start and selectedTotemName then
+                    AutoTotem.Start(ctx, selectedTotemName, distance)
                     ctx.Notify("info", "9X Totem", "Auto spawn totem aktif.", 3)
                 end
             else
